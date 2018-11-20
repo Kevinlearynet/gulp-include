a8f3aca937496751e13a7b5a758ffc4393d5313f
Kevin Leary <info@kevinleary.net>
2018-11-20 11:46:22 -0500

    fix

--- index.js
+++ index.js
@@ -271,8 +271,13 @@ module.exports = function( params ) {
 		}
 
 		// Check content for syntax errors
-		var errors = esprima.parse( content, { tolerant: false } ).errors;
-		console.log( errors );
+		var errors = esprima.parseScript( content, { tolerant: true } ).errors;
+		if ( errors && errors.length > 0 ) {
+			throw new PluginError( 'gulp-include', errors.join( " -- " ), {
+				fileName: filePath,
+				showStack: true
+			} );
+		}
 
 		return { content: content, map: map ? map.toString() : null };
 	}
